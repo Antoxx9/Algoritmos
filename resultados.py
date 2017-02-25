10ca41d6ca621b37dca91d6a3816c2e02e1075c2
Eliot Diaz <eliot.daz@gmail.com>
2017-02-25 18:32:22 -0400

    Redondeo a dos digitos de los resultados de desviacion.

--- resultados.py
+++ resultados.py
@@ -5,9 +5,13 @@ optimo = []
 resultados = []
 desviacion = [] 
 desviacionTotal = 0.0
+#optimo_total = 0
 
 for line in file_opt:
 	optimo.append(float(line))
+	#optimo_total += int(line)
+#print(optimo_total)
+
 file_opt.close()
 
 for line in file_result:
@@ -20,11 +24,11 @@ file_desv = open(name, 'w')
 for i in range(0,len(optimo)):
 	if (optimo[i] != 0):
 		desviacion.append(100 * ((optimo[i] - resultados [i]) / optimo[i]))
-		desviacionTotal += desviacion[i]
+		desviacion[i] = round(desviacion[i], 2)
 	else:
 		desviacion.append(100 * (optimo[i] - resultados [i]))
-		#desviacionTotal += desviacion[i]
-		file_desv.write("Optimo Cero: ")
+		desviacion[i] = round(desviacion[i], 2)	
+	desviacionTotal += desviacion[i]
 	file_desv.write(str(desviacion[i]) + "\n")
 
 file_desv.write("Desviacion Total: " + str(desviacionTotal))
